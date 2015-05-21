6e7f2e6bca04797bdee0ae563d8e64fc170e91e8
Chris Russ <chruss@gmx.de>
2015-05-21 11:15:15 +0200

    initial freelan configurator commit

---
+++ flc.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+
+from __future__ import print_function
+
+import os, sys
+
+def main(argv):
+    print("freelan configurator")
+
+
+if __name__ == "__main__":
+    main(sys.argv[1:])
