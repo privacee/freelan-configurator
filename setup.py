192dfce316f29113989348a53bdb34aaa316baa2
Chris Russ <chruss@gmx.de>
2015-05-28 19:52:50 +0200

    setup.py fix

--- setup.py
+++ setup.py
@@ -8,15 +8,14 @@ setup(
     license='MIT',
     author='Christoph Russ',
     author_email='chruss@gmx.de',
-    keywords='freelan vpn config file',
     url='https://github.com/privacee/freelan-configurator',
-    download_url=''
-    keywords = ['freelan', 'config', 'files']
+    download_url='',
+    keywords = ['freelan', 'vpn', 'config', 'file'],
     zip_safe=False,
     packages=['freelan-configurator'],
     #scripts=['freelan_cmd.py'],
     #entry_points={'console_scripts': ['freelan_cfg = freelan_cfg:main']},
-    install_requires=['appdirs', 'python_dateutil'],
+    install_requires=['appdirs'],
     classifiers=[
         'Environment :: Console',
         'License :: MIT'
