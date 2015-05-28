c97eef2c358c5349cd104ce156396968a0ba213c
Chris Russ <chruss@gmx.de>
2015-05-28 19:50:11 +0200

    preparing release of v0.1

--- setup.py
+++ setup.py
@@ -1,19 +1,21 @@
 import os
-from setuptools import setup, find_packages
+from setuptools import setup
 
 setup(
     name='freelan-configurator',
     version='0.1',
-    description='generating freelan config files for you',
+    description='Generating freelan config files for you.',
     license='MIT',
     author='Christoph Russ',
     author_email='chruss@gmx.de',
     keywords='freelan vpn config file',
     url='https://github.com/privacee/freelan-configurator',
+    download_url=''
+    keywords = ['freelan', 'config', 'files']
     zip_safe=False,
-    packages=find_packages(),
-    scripts=['freelan_cmd.py'],
-    entry_points={'console_scripts': ['freelan_cfg = freelan_cfg:main']},
+    packages=['freelan-configurator'],
+    #scripts=['freelan_cmd.py'],
+    #entry_points={'console_scripts': ['freelan_cfg = freelan_cfg:main']},
     install_requires=['appdirs', 'python_dateutil'],
     classifiers=[
         'Environment :: Console',
